10e6ebfd90723838761db41327773f859b691fef
AntiParty <75674175+AntiParty@users.noreply.github.com>
2022-05-19 02:00:27 -0600

    Add files via upload

---
+++ MuckMod/MyMod.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using MelonLoader;
+
+namespace pluggg
+{
+    public class Main : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F10))
+            {
+                UnityEngine.Object.FindObjectOfType<PlayerStatus>().hp = 999;
+                UnityEngine.Object.FindObjectOfType<PlayerStatus>().maxHp = 999;
+                UnityEngine.Object.FindObjectOfType<PlayerStatus>().stamina = 999;
+                UnityEngine.Object.FindObjectOfType<PlayerStatus>().maxStamina = 999;
+                UnityEngine.Object.FindObjectOfType<PlayerManager>().SetArmor(3,2);
+                UnityEngine.Object.FindObjectOfType<LocalPlayer>().SendMessage("test");
+            }
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                UnityEngine.Object.FindObjectOfType<PlayerManager>().DamagePlayer(1);
+            }
+            if (Input.GetKeyDown(KeyCode.F8)) {
+                UnityEngine.Object.FindObjectOfType<PlayerMovement>().Movement(-999, 999);
+                UnityEngine.Object.FindObjectOfType<PowerupInventory>().GetExtraJumps();
+            }
+            if (Input.GetKeyDown(KeyCode.F7)) {
+                UnityEngine.Object.FindObjectOfType<PlayerStatus>().strength = 999;
+                UnityEngine.Object.FindObjectOfType<InventoryItem>().attackDamage = 999;
+                UnityEngine.Object.FindObjectOfType<InventoryItem>().attackSpeed = 999;
+                UnityEngine.Object.FindObjectOfType<InventoryItem>().attackRange = 999;
+            }
+            if (Input.GetKeyDown(KeyCode.F6)) {
+
+            }
+
+        }
+        private void Awake()
+        {
+            
+        }
+
+        public string username;
+
+        public static bool flyMode;
+    }
+}
